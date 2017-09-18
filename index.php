d6904189df965f0315f1a7a68aa0e2d72b8efa66
keyide <1543146382@qq.com>
2017-09-18 17:45:59 +0800

    first

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+    header('content-type:text/html; charset=utf8;');
+
+    //echo '<div>123</div>';
+    //include('/views/main/login.html');
+
+    //var_dump($path);
+
+
+    $dir='main';
+    $fileName='index';
+
+    if(array_key_exists('PATH_INFO',$_SERVER)){
+
+        $path = $_SERVER['PATH_INFO'];
+        $str=substr($path,1);
+        $strArr=explode('/',$str);
+        if(count($strArr)==2){
+            $dir=$strArr[0];
+            $fileName=$strArr[1];
+        }else{
+            $fileName='login';
+        }
+
+    }
+    include('/views/'.$dir.'/'.$fileName.'.html');
+
+?>
